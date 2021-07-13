8b6ac184b725dbbae8d2214495f3cc4ae1587093
chrisWalker11 <51098965+chrisWalker11@users.noreply.github.com>
2021-07-13 09:05:33 -0400

    Delete java fall2020 directory

--- java fall2020/notes/First.java
+++
@@ -1,7 +0,0 @@
-//First program in java
-
-public class First {
-  public static void main(String [] args) {
-    System.out.println("Hello world");
-  }
-}
--- java fall2020/notes/Second.java
+++
@@ -1,5 +0,0 @@
-public class Second { // comments second class
-  public static void main(String[] args) {
-System.out.println("Second message");
-  }
-}
